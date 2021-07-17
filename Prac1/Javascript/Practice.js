46302237017456933942c0bea8aa048773a2c730
ash <ad26_4@hotmail.com>
2021-07-17 14:54:19 +1000

    test

---
+++ Prac1/Javascript/Practice.js
@@ -0,0 +1,13 @@
+
+var scores = [];
+var average = 0;
+
+function UpdateAverageScore() {
+	average = 0;
+	scores.push(document.getElementById("score").value)
+	for (var i = 0; i < scores.length; i++) {
+		average += (scores[i] / 1);
+	}
+	average = average / scores.length;
+	document.getElementById("scoreAverage").innerHTML = average;
+}
